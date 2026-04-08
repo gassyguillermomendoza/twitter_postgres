c2931f386fd976a0653215eacaa7925776a23219
gassyguillermomendoza <gmendoza@students.pitzer.edu>
2026-04-07 23:21:22 -0700

    final?

--- load_tweets.sh
+++ load_tweets.sh
@@ -2,7 +2,7 @@
 
 # list all of the files that will be loaded into the database
 # for the first part of this assignment, we will only load a small test zip file with ~10000 tweets
-# but we will write are code so that we can easily load an arbitrary number of files
+# but we will write our code so that we can easily load an arbitrary number of files
 files='
 test-data.zip
 '
@@ -10,9 +10,13 @@ test-data.zip
 echo 'load normalized'
 for file in $files; do
     # call the load_tweets.py file to load data into pg_normalized
+    python3 load_tweets.py --db "postgresql://postgres:pass@localhost:10824/postgres" --inputs "$file"
 done
+
 
 echo 'load denormalized'
 for file in $files; do
-    # use SQL's COPY command to load data into pg_denormalized
+    unzip -p $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:10823/postgres -c "\COPY tweets_jsonb (data) FROM STDIN CSV QUOTE e'\x01' DELIMITER e'\x02'"
+
 done
+
